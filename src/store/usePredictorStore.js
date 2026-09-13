fb9f03b3700b712aa4bb3ec5b77862f2457947e1
EmergingCreator <creatoremerging@gmail.com>
2026-09-13 17:08:12 +0530

    Update frontend API

--- src/store/usePredictorStore.js
+++ src/store/usePredictorStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
  
-const API_URL = "https://titanic-backend-kvqv.onrender.com//predict";
+const API_URL = "https://titanic-backend-kvqv.onrender.com/predict";
  
 
 export const usePredictorStore = create((set, get) => ({
